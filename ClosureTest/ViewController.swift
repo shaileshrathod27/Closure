c2e6827cc2737fec5ad62a0d71d7ed2cfc5c38d1
ChavdaKishan <chavdakish1996@gmail.com>
2017-06-13 11:12:22 +0530

    4

--- ClosureTest/ViewController.swift
+++ ClosureTest/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        var customersInLine = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+        print(customersInLine.count)
+        
+        let customerProvider = {customersInLine.remove(at: 0)}
+        print(customersInLine.count)
+        
+        print("Now serving \(customerProvider())!")
+        // Prints "Now serving Chris!"
+        print(customersInLine.count)
+        // Prints "4"
+        
+        
+        networkCall { (value) -> (Float) in
+            print("valueInt ==>> \(value)")
+            return 1.0
+        }
+        
+        var objStudent = Student(name: "Anil")
+        print("Name 1 ==>> ", objStudent.name)
+
+        testInOut(value: &objStudent)
+        print("Name 2 ==>> ", objStudent.name)
+        
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func networkCall(completionHandler : @escaping (Int)->(Float))->Void
+    {
+        print("Process Goes here")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0, execute: {
+            //Process Goes here
+            let valueFloat = completionHandler(10)
+            print("valueFloat ==>> \(valueFloat)")
+        })
+    }
 
-
+    struct Student {
+        var name : String
+    }
+    
+    func testInOut( value : inout Student){
+        //var value = value
+        value.name = "Hello"
+    }
 }
-
